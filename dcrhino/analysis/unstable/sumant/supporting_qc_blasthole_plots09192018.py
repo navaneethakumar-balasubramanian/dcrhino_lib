c81776617f6930984ee87de6d7bfa1defdd276de
Karl N. Kappler <kkappler@quakefinder.com>
2018-09-25 15:40:32 -0700

    minor changes

--- dcrhino/analysis/unstable/sumant/supporting_qc_blasthole_plots09192018.py
+++ dcrhino/analysis/unstable/sumant/supporting_qc_blasthole_plots09192018.py
@@ -210,7 +210,7 @@ def fill_nan(A):
     '''
     inds = np.arange(A.shape[0])
     good = np.where(np.isfinite(A))
-    f = interpolate.interp1d(inds[good], A[good],bounds_error=False)
+    f = interpolate.interp1d(inds[good], A[good], bounds_error=False)
     B = np.where(np.isfinite(A),A,f(inds))
     return B
 
