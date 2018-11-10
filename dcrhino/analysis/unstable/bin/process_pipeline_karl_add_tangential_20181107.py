717189fbd68b33ab8e7d37211ea84558a5e63b0b
Karl N. Kappler <kkappler@quakefinder.com>
2018-11-09 16:20:50 -0800

    commit before add rop cull

--- dcrhino/analysis/unstable/bin/process_pipeline_karl_add_tangential_20181107.py
+++ dcrhino/analysis/unstable/bin/process_pipeline_karl_add_tangential_20181107.py
@@ -44,11 +44,17 @@ from dcrhino.process_pipeline.qc_log_plotter import QCLogPlotterv2
 #from process_pipeline_functions import apply_calibration, interpolate_data, autocorrelate_trace
 #from process_pipeline_functions import deconvolve_trace, correlate_trace, bandpass_filter_trace,
 from dcrhino.process_pipeline.process_pipeline_functions import trim_trace, get_features_extracted
+from dcrhino.analysis.signal_processing.mwd_tools import reject_traces_with_small_rop
+
 print('wtf')
 SHEAR_VELOCITY = 2654
 ACOUSTIC_VELOCITY = 4755
 mine = 'line_creek';
 mine = 'WEST_ANGELAS';
+component = 'tangential'
+#component = 'axial'
+plot_depth = 1#False
+
 if mine=='line_creek':
     data_path = '/home/kkappler/data/datacloud/rhino_process_pipeline_output/line_creek/5208/3200'
     hole_uids = ['793-MR_77-23531', '793-MR_77-23631', '793-MR_77-23731',
@@ -56,17 +62,17 @@ if mine=='line_creek':
                  '793-MR_77-24130',]
     sampling_rate = 3200.0
     sensor_distance_to_source = 20.967872
-    every_nth = 7
+    every_nth = 2
 elif mine=='WEST_ANGELAS':
     data_path = os.path.join('/home/kkappler/data/datacloud/rhino_process_pipeline_output/WEST_ANGELAS/5208/4000/')
     hole_uids = ['710-197-3/2018-10-18_00001', ]
     sensor_distance_to_source = 16.37
     sampling_rate = 4000.0
-    every_nth = 4
+    every_nth = 1#4
 dt= 1./sampling_rate
-normalize_traces_for_plotting = True
+normalize_traces_for_plotting = 1# False
 processing_trace_decimation_factor = 7# integer
-do_processing = False
+do_processing = 1#False
 #<boilerplate config file settings>
 #[PROCESSING]
 deconvolution_filter_duration = 0.1
@@ -80,7 +86,7 @@ trapezoidal_bpf_duration = 0.02
 spiking_decon_filter_duration = 0.010 #10ms; parameterize in terms of trace length
 start_ms_despike_decon = 3.0
 end_ms_despike_decon = 70.0
-add_noise_percent = 0#200.0
+add_noise_percent = 200.0
 multiple_window_search_width_ms = 3.126
 #</boilerplate config file settings>
 corners = [trapezoidal_bpf_corner_1, trapezoidal_bpf_corner_2,
@@ -115,30 +121,32 @@ def get_axial_tangential_radial_traces(hole_uid, flavour, trace_decimation=None)
 
 
 
+
 hole_uid = hole_uids[0]
+
+mwd_df = pd.read_csv(os.path.join(data_path, hole_uid, 'hole_mwd.csv'))
+features_df = pd.read_csv(os.path.join(data_path, hole_uid, 'extracted_features.csv'))
+#features_df = reject_traces_with_small_rop(features_df)#, threshold=0.006):
+depth = np.asarray(features_df.depth)
 interpolated_traces = get_axial_tangential_radial_traces(hole_uid, 'interpolated')
 deconvolved_traces = get_axial_tangential_radial_traces(hole_uid, 'deconvolved')
 unfiltered_correlated_traces = init_trace_dict()
 filtered_despiked_traces = init_trace_dict()
 filtered_correlated_traces = get_axial_tangential_radial_traces(hole_uid, 'filtered_correlated')
-component = 'tangential'
-component = 'axial'
 middle = int((1+deconvolution_filter_duration)*sampling_rate/2 )
 n_back = int(sampling_rate * min_lag_trimmed_trace)
 n_advance = int(sampling_rate * max_lag_trimmed_trace)
 #pdb.set_trace()
 plotter_data = filtered_correlated_traces[component][0::every_nth,middle+n_back:middle+n_advance].T
 
-mwd_df = pd.read_csv(os.path.join(data_path, hole_uid, 'hole_mwd.csv'))
-features_df = pd.read_csv(os.path.join(data_path, hole_uid, 'extracted_features.csv'))
 
 
-print("despiing decon")
+print("despiking decon")
 pdb.set_trace()
 unfiltered_correlated_traces[component] = np.full(filtered_correlated_traces[component].shape, np.nan)
 filtered_despiked_traces[component] = np.full(filtered_correlated_traces[component].shape, np.nan)
 n_traces, n_samples_per_trace = interpolated_traces[component].shape
-
+n_spiking_decon_filter_taps
 if do_processing:
     for i_trace in range(n_traces):
         print(i_trace)
@@ -160,6 +168,7 @@ if do_processing:
                                                           add_noise_percent=add_noise_percent)
         filtered_despiked_trace = ssig.filtfilt(fir1_taps, 1., despiked_trace).astype('float32')
         filtered_despiked_traces[component][i_trace,:] = filtered_despiked_trace
+        filtered_despiked_traces[component][i_trace,:] = np.roll(filtered_despiked_trace, n_spiking_decon_filter_taps//2)
         #pdb.set_trace()
     #np.save(os.path.join(data_path, hole_uid, 'trimmed_filtered_correlated_traces.npy'), trimmed_filtered_correlated_traces)
     filtered_despiked_filebase = '{}_filtered_despiked_traces.npy'.format(component)
@@ -172,7 +181,7 @@ filtered_despiked_traces[component] = np.load(os.path.join(data_path, hole_uid, 
 despiked_plotter_data = filtered_despiked_traces[component][0::every_nth,middle+n_back:middle+n_advance].T
 qq = np.max(plotter_data, axis=0)/np.max(despiked_plotter_data, axis=0)
 despiked_plotter_data *= qq
-normalize_traces_for_plotting = True
+
 if normalize_traces_for_plotting:
     divvy = plotter_data.max(axis=0)
     plotter_data /= divvy
@@ -180,9 +189,16 @@ if normalize_traces_for_plotting:
     despiked_plotter_data /= despiked_divvy
 #</normalize traces before plotting AND see if multple better>
 plt.figure(1)
-plt.subplot(2, 1, 1)
-seismic_wiggle(plotter_data, dt=dt, scale=1.0, color='grey', time_shift=-0.1, min_t = -0.01, max_t = 0.04 )
-plt.title("{}, {}, {} Without Despike".format(mine, hole_uid, component))
+ax1 = plt.subplot(2, 1, 1)
+if plot_depth:
+    seismic_wiggle(plotter_data, dt=dt, scale=1.0, color='grey', time_shift=-0.1,
+                   min_t = -0.01, max_t = 0.04, depth=depth[0::every_nth], ranges=(depth[0], depth[-1]) )
+else:
+    seismic_wiggle(plotter_data, dt=dt, scale=1.0, color='grey', time_shift=-0.1, min_t = -0.01, max_t = 0.04 )
+
+ttl_string = "{}, {}, {} Without Despike, Normalized={}".format(mine, hole_uid,
+              component, normalize_traces_for_plotting)
+plt.title(ttl_string)
 primary_horiz_line = 0.0
 if component == 'tangential':
     multiple_horiz_line = primary_horiz_line + 2*sensor_distance_to_source/SHEAR_VELOCITY
@@ -195,11 +211,17 @@ plt.hlines([primary_horiz_line + 0.001*start_ms_despike_decon],0, plotter_data.s
 plt.hlines([primary_horiz_line + 0.001*end_ms_despike_decon],0, plotter_data.shape[1], color='black', linewidth = 1.0, alpha=alpha)
 plt.ylabel('time (s)')
 
-plt.subplot(2, 1, 2)
+ax2 = plt.subplot(2, 1, 2, sharex=ax1, sharey=ax1)
 #plt.figure(2)
-seismic_wiggle(despiked_plotter_data, dt=dt, scale=1.0, color='k')
+#seismic_wiggle(despiked_plotter_data, dt=dt, scale=1.0, color='k')
+if plot_depth:
+    seismic_wiggle(despiked_plotter_data, dt=dt, scale=1.0, color='black', time_shift=-0.1,
+               min_t = -0.01, max_t = 0.04, depth=depth[0::every_nth], ranges=(depth[0], depth[-1]) )
+else:
+    seismic_wiggle(despiked_plotter_data, dt=dt, scale=1.0, color='black', time_shift=-0.1,
+               min_t = -0.01, max_t = 0.04)
 plt.title("With Despike")
-primary_horiz_line = 0.1
+primary_horiz_line = 0.0# - spiking_decon_filter_duration/2.0
 multiple_horiz_line = primary_horiz_line + 2*sensor_distance_to_source/SHEAR_VELOCITY
 alpha=0.5
 plt.hlines([multiple_horiz_line,],0, plotter_data.shape[1], color='lime', linewidth = 3.3, alpha=alpha)
@@ -207,8 +229,10 @@ plt.hlines([primary_horiz_line,], 0, plotter_data.shape[1], color='yellow', line
 plt.hlines([primary_horiz_line + 0.001*start_ms_despike_decon],0, plotter_data.shape[1], color='black', linewidth = 1.0, alpha=alpha)
 plt.hlines([primary_horiz_line + 0.001*end_ms_despike_decon],0, plotter_data.shape[1], color='black', linewidth = 1.0, alpha=alpha)
 plt.ylabel('time (s)')
-plt.show()
-plt.xlabel('trace')
+if plot_depth:
+    plt.xlabel('depth (m)')
+else:
+    plt.xlabel('trace')
 plt.show()
 #data = data[2100+clip_upper:2400-clip_lower, 0::plot_every]
 
