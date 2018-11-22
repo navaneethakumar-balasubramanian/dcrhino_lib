29bcfeb698710a686e940ad9468802433ebb1439
Karl N. Kappler <kkappler@quakefinder.com>
2018-11-22 13:46:48 -0800

    cleanup re: JIRA59

--- dcrhino/analysis/graphical/test_spectral_qc_plot.py
+++ dcrhino/analysis/graphical/test_spectral_qc_plot.py
@@ -29,15 +29,21 @@ def make_spectral_qc_plot(h5_helper, components_to_plot=['x', 'y', 'z'],
     @TODO: @Natal: can we make trace_duration_in_seconds depend on
     h5_helper.metadata.trace_length; currently returns None
 
-    @ivar v_min, v_max: min/max values for coloraxislims
+    @ivar vmin, vmax: min/max values for coloraxislims
     @ivar header_frequency_band, list (or array) with two elements, the low and high
     cut edges of the band of interest.  Within this band we pick the maximum
     spectral amplitude for each trace and plot it.
 
 
     """
+    header_color_scheme = {}
+    header_color_scheme['x'] = 'blue'
+    header_color_scheme['y'] = 'red'
+    header_color_scheme['z'] = 'green'
     h5h = h5_helper
     h5_basename = os.path.basename(h5_helper.h5f.filename)
+    meta_dict = h5_helper.metadata.metadata_to_dictionary()
+    header_frequency_band_str = '{}-{}Hz'.format(header_frequency_band[0],header_frequency_band[1])
 
     num_pcolor_plot_panels = len(components_to_plot)
     samples_per_second = float(h5h.metadata.output_sampling_rate)#3200.0
@@ -62,7 +68,7 @@ def make_spectral_qc_plot(h5_helper, components_to_plot=['x', 'y', 'z'],
     f_keep_indices = np.where(frq <= frequency_high_cut)[0]
     frq = frq[f_keep_indices]
     band_indices = np.where((frq>=header_frequency_band[0]) & (frq<=header_frequency_band[1]))[0]
-
+    #max_spectral_info_dict = {}
     if 'x' in components_to_plot:
         Sxx = Sxx[f_keep_indices,:]
         max_xx = np.max(Sxx, axis=0)
@@ -83,70 +89,87 @@ def make_spectral_qc_plot(h5_helper, components_to_plot=['x', 'y', 'z'],
         max_zz_band = np.log10(max_zz_band)
 
 
-
-    fig = plt.figure(22, figsize = (8.5, 11), dpi=300)
-    plt.subplot(num_pcolor_plot_panels+1, 1, 1)
+    fig, ax = plt.subplots(num_pcolor_plot_panels+1, figsize = (8.5, 11),
+                           dpi=300, sharex=True)
+    #fig = plt.figure(22, figsize = (8.5, 11), dpi=300)
+    #plt.subplot(num_pcolor_plot_panels+1, 1, 1)
     #<plot header>
+    #for xyz in components_to_plot:
+    header_linewidth = 0.7
     if 'x' in components_to_plot:
-        plt.plot(t_axis, max_xx, label='global max x')
-        plt.plot(t_axis, max_xx_band, label='100-300Hz max x')
+        xyz='x'
+        ax[0].plot(t_axis, max_xx, label='\nglobal max x', color=header_color_scheme['x'], linewidth=header_linewidth)
+        ax[0].plot(t_axis, max_xx_band, label='in-band max x', color=header_color_scheme['x'],
+          linewidth=header_linewidth, linestyle='--')#, alpha=0.5)
     if 'y' in components_to_plot:
-        plt.plot(t_axis, max_yy, label='global max y')
-        plt.plot(t_axis, max_yy_band, label='100-300Hz max y')
+        ax[0].plot(t_axis, max_yy, label='global max y', color=header_color_scheme['y'], linewidth=header_linewidth)
+        #ax[0].plot(t_axis, max_yy_band, label='in-band max y')
+        ax[0].plot(t_axis, max_yy_band, label='in-band max y', color=header_color_scheme['y'],
+          linestyle='--', linewidth=header_linewidth)#, alpha=0.5)
     if 'z' in components_to_plot:
-        plt.plot(t_axis, max_yy, label='global max z')
-        plt.plot(t_axis, max_yy_band, label='100-300Hz max z')
-    plt.legend()
+        ax[0].plot(t_axis, max_zz, label='global max z', color=header_color_scheme['z'], linewidth=header_linewidth)
+        ax[0].plot(t_axis, max_zz_band, label='in-band max z', color=header_color_scheme['z'],
+          linestyle='--', linewidth=header_linewidth)#, alpha=0.5)
+        #ax[0].plot(t_axis, max_zz_band, label='in-band max z')
+    #ax[0].legend(framealpha=0.1)
+    #ax[0].legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., framealpha=0.1)
+    ax[0].legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+       ncol=3, mode="expand", borderaxespad=0.)
+
+    ttl_str = '{}, orientation={}'.format(h5_basename[:-3], meta_dict['orientation'] )
+    ttl_str = '{}, band = {}'.format(ttl_str, header_frequency_band_str)
+    #ax[0].set_title(ttl_str)
+    plt.suptitle(ttl_str)
     #</plot header>
-    i_panel = 2
-    while i_panel <= num_pcolor_plot_panels + 1:
-        plt.subplot(num_pcolor_plot_panels+1, 1, i_panel)
+    i_panel_index = 1
+    while i_panel_index <= num_pcolor_plot_panels:
+        #ax[i_panel_index].subplot(num_pcolor_plot_panels+1, 1, i_panel)
         component_to_plot = components_to_plot.pop(0)
         if component_to_plot =='x':
-            plt.pcolormesh(t_axis, frq, np.log10(Sxx));
+            im = ax[i_panel_index].pcolormesh(t_axis, frq, np.log10(Sxx), vmin=vmin, vmax=vmax);
         elif component_to_plot == 'y':
-            plt.pcolormesh(t_axis, frq, np.log10(Syy));
+            im = ax[i_panel_index].pcolormesh(t_axis, frq, np.log10(Syy), vmin=vmin, vmax=vmax);
         elif component_to_plot == 'z':
-            plt.pcolormesh(t_axis, frq, np.log10(Szz));
-        plt.clim(vmin=vmin, vmax=vmax)
-        plt.title('component {}'.format(component_to_plot))
-        plt.ylabel('Frequecny [Hx]')
-        plt.xlabel('Trace index ({}[s])'.format(trace_duration_in_seconds))
-        i_panel+=1
+            im = ax[i_panel_index].pcolormesh(t_axis, frq, np.log10(Szz), vmin=vmin, vmax=vmax);
+        #pdb.set_trace()
+        #plt.clim(vmin=vmin, vmax=vmax)
+        ax[i_panel_index].set_title('component {}'.format(component_to_plot))
+        ax[i_panel_index].set_ylabel('Frequecny [Hx]')
+        i_panel_index+=1
+    ax[i_panel_index-1].set_xlabel('Trace index ({}[s])'.format(trace_duration_in_seconds))
+
+    #fig.colorbar(orientation="vertical", pad=0.35)
+    #fig.colorbar(im, orientation="horizontal", pad=0.1)
+    #ax[i_panel_index-1].colorbar()#im, orientation="horizontal", pad=0.1)
+    cbaxes = fig.add_axes([0.01, 0.1, 0.007, 0.8])
+    cb = plt.colorbar(im, cax = cbaxes)
+    plt.setp(cbaxes.get_xticklabels(), rotation='vertical', fontsize=10)
+    cbaxes.yaxis.set_ticks_position('right')
     #plt.colorbar()
-    #plt.subplot(4,1,4)
-    #plt.pcolormesh(t, f, np.log10(Szz));
-    #plt.clim(vmin=-5, vmax=0.0)
-    #plt.savefig('tangential_component.png')
-    #cbaxes = fig.add_axes([0.8, 0.1, 0.03, 0.8])
-    #cb = plt.colorbar(ax1, cax = cbaxes)
-    plt.savefig('spectra_{}.png'.format(h5_basename[:-3]))
-    pdb.set_trace()
-    plt.show()
-#n_bins = 30
-#frq = np.logspace(-0.9, 3.0, n_bins+1)
-#Sxx_binned  = np.full((n_bins, num_traces), np.nan)
-#for i_bin in range(n_bins):
-#    bin_indices = np.where((f>=frq[i_bin]) & (f<=frq[i_bin+1]))[0]
-#    binned_data = Sxx[bin_indices,:]
-#    avg_bin = np.mean(binned_data,axis=0)
-#    pdb.set_trace()
-#    Sxx_binned[i_bin,:] = avg_bin;#np.mean(Sxx[bin_indices,:], axis=0)
-#    #print(Sxx_binned)
-#    pdb.set_trace()
+    #<save figure>
+    output_path = ''
+    output_file_basename = 'spectra_{}.png'.format(h5_basename[:-3])
+    full_output_file = os.path.join(output_path, output_file_basename)
+    plt.savefig(full_output_file)
+    #<save figure>
+    #plt.show()
+
 
 
 
 def main():
     """
     """
+    use_debug_file = True
+    #pdb.set_trace()
     h5_basename = '20181119_SSX54322_5209_3200.h5'
     mine_path = os.path.join(home, 'data/datacloud/field_data/detour_gold/detour_lake_mine')
     h5_path = os.path.join(mine_path,'level_1/piezo/3200hz', h5_basename)
-
+    if use_debug_file:
+        h5_path = os.path.join(home, 'data/datacloud/debug/run_1542066345/20181112_RTR85545_S1021.h5')
     h5 = h5py.File(h5_path)
     h5_helper = H5Helper(h5)
-    #pdb.set_trace()
+    pdb.set_trace()
     make_spectral_qc_plot(h5_helper)# components_to_plot=['x', 'y', 'z'])
 
     print("finito {}".format(datetime.datetime.now()))
