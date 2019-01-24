aba41f277956b8265cb0b2de679a5478fdc14fcb
Karl N. Kappler <kkappler@quakefinder.com>
2019-01-23 16:09:27 -0800

    commit with notes on finishing this --- need to switch to arcelorjob

--- bin/test.py
+++ bin/test.py
@@ -5,14 +5,16 @@ Created on Mon Jan 21 16:10:30 2019
 
 @author: thiago
 """
+import json
+import numpy as np
+import pdb
 
 from dcrhino3.helpers.rhino_db_helper import RhinoDBHelper
 from dcrhino3.helpers.mwd_helper import MWDHelper
-from dcrhino3.models.trace_dataframe import TraceData
 from dcrhino3.helpers.mwd_rhino_merger import MWDRhinoMerger
 from dcrhino3.models.env_config import EnvConfig
-import numpy as np
-import pdb
+from dcrhino3.models.trace_dataframe import TraceData
+
 
 mine_name = 'milligan'
 envConfig = EnvConfig()
@@ -31,7 +33,8 @@ if conn is not False:
 
     line = matches.iloc[0]
     hole_mwd = mwd_helper.get_hole_mwd_from_mine_mwd(mwd_df, line.bench_name,
-                                                     line.pattern_name,line.hole_name,line.hole_id)
+                                                     line.pattern_name,line.hole_name,
+                                                     line.hole_id)
     files_ids = np.array(line.files_ids.split(','))
     min_ts = int((hole_mwd['start_time'].astype(int)/1000000000).min())
     max_ts = int((hole_mwd['start_time'].astype(int)/1000000000).max())
@@ -41,6 +44,11 @@ if conn is not False:
 
     #pdb.set_trace()
     acor_trace.dataframe = merger.merge_mwd_with_trace(hole_mwd,acor_trace.dataframe)
+    file_id = acor_trace.dataframe['acorr_file_id'].unique()[0]
+    print('warning! assuming unique above - need to add logic to check this')
+    unicode_string = db_helper.get_file_config(14, 1)
+    config_dict = json.loads(unicode_string)
+    print('save the unicode string as an attr')
     h5_path = 'test3.h5'
     acor_trace.save_to_h5(h5_path)
     #pdb.set_trace()
