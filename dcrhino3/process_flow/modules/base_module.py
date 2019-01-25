bda44593ff4a8e2e59498428c61bed94e605ca68
Karl N. Kappler <kkappler@quakefinder.com>
2019-01-25 10:12:30 -0800

    doc

--- dcrhino3/process_flow/modules/base_module.py
+++ dcrhino3/process_flow/modules/base_module.py
@@ -2,36 +2,47 @@
 
 
 import json
+
 class BaseModule(object):
-    def __init__(self,json,output_path):
+    def __init__(self, json, output_path):
+        """
+        @type json: dictionary
+        @iver args: dictionary, from json, this is where we specify the numerical
+        and other specific values and parameters for the process module instance
+        for example, if adding 3.0 to the traces, the number 3.0 would be in here
+        """
         self.id = "base_module"
         self.version = 1
         self.json = json
         self.output_path = output_path
-        
+
         self.output_to_file = False
         self.args = {}
-    
+
         self.set_data_from_json(json)
-    
-    def get_transformed_args(self,global_config):
+
+
+    def get_transformed_args(self, global_config):
         transformed = dict()
         for key in self.args.keys():
             val = self.args[key]
             if (type(val) == unicode or type(val) == str) and "|global_config." in str(val):
                 gc_var_name = val.replace("|","").replace("global_config.","")
-                transformed[key] = getattr(global_config,gc_var_name)
+                transformed[key] = getattr(global_config, gc_var_name)
             else:
                 transformed[key] = val
         return transformed
-        
+
     def set_data_from_json(self,json):
+        """
+        managing the json, making accessible
+        """
         for _key in json.keys():
             self.__dict__[_key] = json[_key]
-            
-    def output_to_file(self):
-        return ('output_to_file' in self.args.keys() and self.args['output_to_file'] == True)
-    
+
+#    def output_to_file(self):
+#        return ('output_to_file' in self.args.keys() and self.args['output_to_file'] == True)
+
     def applied_module_string(self,args):
         temp = dict()
         temp['module_id'] = self.id
--- dcrhino3/process_flow/modules/trace_processing/base_trace_module.py
+++ dcrhino3/process_flow/modules/trace_processing/base_trace_module.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import pdb
-import json
+#import json
 from dcrhino3.process_flow.modules.base_module import BaseModule
 
 class BaseTraceModule(BaseModule):
-    def __init__(self,json,output_path):        
-        BaseModule.__init__(self,json,output_path)
+    def __init__(self, json, output_path):
+        BaseModule.__init__(self, json, output_path)
         self.id = "base_trace_module"
-            
-        
+
+
     def process_trace(self,trace,args=None):
         output_df = trace.dataframe.copy()
-        
+
         for line_idx in range(len(output_df)):
             trace_config = trace.global_config_by_index(output_df.iloc[line_idx]['acorr_file_id'])
             for component_id in trace_config.components_to_process:
@@ -20,7 +20,7 @@ class BaseTraceModule(BaseModule):
                 trace_to_process = output_df.iloc[line_idx][component_column_on_df]
                 processed_trace = self.process_component(component_id,trace_to_process,trace_config)
                 output_df.at[line_idx,component_column_on_df] = processed_trace
-        
+
         trace.dataframe = output_df
 
         trace.add_applied_module(self.applied_module_string(args))
@@ -29,8 +29,12 @@ class BaseTraceModule(BaseModule):
 #            trace.save_to_h5(self.output_path)
 
         return trace
-    
+
     def process_component(self,component_id,component_array,global_config):
+        """
+        this function is to be overwritten on each Module class.  It is basically
+        the "make" method for the data processing
+        """
         return component_array
 
 
