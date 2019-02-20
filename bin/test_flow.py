4a290cff250eb6d57d6f310d54a49c289c2ee86a
Thiago Mourao <xtecox@gmail.com>
2019-02-20 15:09:34 -0300

    accepting txt files list as input

--- bin/test_flow.py
+++ bin/test_flow.py
@@ -23,6 +23,7 @@ from dcrhino3.helpers.general_helper_functions import init_logging
 logger = init_logging(__name__)
 
 def process_file(process_json,acorr_h5_file_path,env_config):
+    logger.info("PROCESSING FILE:" + str(acorr_h5_file_path))
     #process_flow_path = "/home/thiago/Documents/Projects/Dc_rhino/v3/bin/process_flows/example_simple_flow.json"
     #holes_selection_path = "/home/thiago/Documents/Projects/Dc_rhino/v3/bin/process_flows/holes_selection/example_hole_selection.json"
     #acorr_h5_file_path = "/home/thiago/milligan_cache/interpolated/32555_2235_2235.h5"
@@ -36,7 +37,7 @@ def process_file(process_json,acorr_h5_file_path,env_config):
 
     acorr_trace = TraceData()
     acorr_trace.load_from_h5(acorr_h5_file_path)
-    #acorr_trace.dataframe = acorr_trace.dataframe[:300]
+    #acorr_trace.dataframe = acorr_trace.dataframe[:10]
     filename = os.path.basename(acorr_h5_file_path)
     filename_without_ext = filename.replace(".h5","")
 
@@ -73,14 +74,20 @@ if __name__ == '__main__':
     if not files:
         print  'File does not exist: ' + args.h5_path
     for file in files:
+
+        if ".txt" in os.path.splitext(file)[1]:
+            txt_folder_path = os.path.dirname(file)
+            f = open(file, "r")
+            file_text = f.read()
+            files_in_file = file_text.split("\n")
+            for file_in_file in files_in_file:
+                file_path = os.path.join(txt_folder_path, file_in_file)
+                p = Process(target=process_file, args=(process_json, file_path, env_config))
+                p.start()
+                p.join()
         if '.h5' in os.path.splitext(file)[1]:
-            logger.info("PROCESSING FILE:" + str( file))
+
             if env_config.is_file_blacklisted(file) is False:
                 p = Process(target=process_file, args=(process_json,file,env_config))
                 p.start()
                 p.join()
-                #process_file(process_json,file,env_config)
-
-
-
-
