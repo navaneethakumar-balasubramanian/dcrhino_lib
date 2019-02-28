3933a03b04945da866ffc34c75d7d4d826e4d237
Karl N. Kappler <kkappler@quakefinder.com>
2019-02-28 15:13:19 -0800

    oops fixing committed trace

--- dcrhino3/process_flow/process_flow.py
+++ dcrhino3/process_flow/process_flow.py
@@ -168,7 +168,7 @@ class ProcessFlow:
         for module in self.plotters_flow:
             t0 = time.time()
             logger.info("Plotting using module: " + str(module.id) + " with: " + str(module.args))
-            pdb.set_trace()
+            # pdb.set_trace()
             module.plot_trace_data(output_trace,process_flow_id=self.id)
             delta_t = time.time() - t0
             logger.info("{} ran in {}s ".format(module.id, delta_t))
