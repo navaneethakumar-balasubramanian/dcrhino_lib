28c9fca162c85db4df79ac65d6b5c1187095baca
natal <natal@datacloud.com>
2019-03-06 23:50:04 +0000

    troubleshooting

--- dcrhino3/acquisition/real_time_acquisition_v3.py
+++ dcrhino3/acquisition/real_time_acquisition_v3.py
@@ -243,9 +243,7 @@ class FileFlusher(threading.Thread):
         self.current_timestamp += self.elapsed_tx_sequences * delta_t
         self.sequence = packet.tx_sequence
 
-        print(int(self.current_timestamp), self.previous_second)
         if int(self.current_timestamp) > self.previous_second:
-            pdb.set_trace()
             print("index", self.packet_index_in_trace)
 
         if self.packet_index_in_trace >= sampling_rate:
