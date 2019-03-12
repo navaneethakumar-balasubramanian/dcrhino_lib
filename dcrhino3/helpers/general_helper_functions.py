63d3c3e81468fefec0f246c474b8f29e8751caf5
natal <natal@datacloud.com>
2019-03-12 17:29:28 +0000

    centralized interpolation to general helper functions and using quadratic interpolation

--- dcrhino3/helpers/general_helper_functions.py
+++ dcrhino3/helpers/general_helper_functions.py
@@ -187,6 +187,12 @@ def find_files(directory, pattern, **kwargs):
 
     return matches
 
+def interpolate_data(raw_timestamps,data,ideal_timestamps):
+    # print("Interpolating")
+    interp_function = interp1d(raw_timestamps, data, kind="quadratic", bounds_error=False, fill_value=0)
+    interp_data = interp_function(ideal_timestamps)
+    # interp_data = np.interp(ideal_timestamps,data,raw_timestamps)
+    return interp_data
 
 
 #def arrayBounds(ts,ordered=False,**kwargs):
