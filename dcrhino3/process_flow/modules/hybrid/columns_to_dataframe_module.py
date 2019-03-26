9979ba71ce69ae2a8440bca8617e995667f2944b
Thiago Mourao <xtecox@gmail.com>
2019-03-26 16:15:36 -0300

    Columns to DF

---
+++ dcrhino3/process_flow/modules/hybrid/columns_to_dataframe_module.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 25 11:52:23 2019
+
+@author: thiago
+"""
+
+# -*- coding: utf-8 -*-
+
+import pdb
+from dcrhino3.process_flow.modules.hybrid.base_hybrid_module import BaseHybridModule
+
+
+class ColumnsToDataframeModule(BaseHybridModule):
+    def __init__(self, json, output_path, process_flow, order):
+        BaseHybridModule.__init__(self, json, output_path, process_flow, order)
+        self.id = "columns_to_df"
+        self.can_alter_trace = False
+        self.can_create_columns = False
+
+    def process_splitted_trace(self, splitted_trace):
+        transformed_args = splitted_trace["transformed_args"]
+        for col in transformed_args.__dict__.keys():
+            splitted_trace["dataframe"][col] = transformed_args.__getattribute__(col)
+        return splitted_trace
