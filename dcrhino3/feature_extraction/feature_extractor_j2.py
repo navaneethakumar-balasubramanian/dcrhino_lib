568b78905f907125b0fc7bc1a2a626fa2a1c21ca
Karl N. Kappler <kkappler@quakefinder.com>
2019-03-29 17:47:13 -0700

    add, dev

---
+++ dcrhino3/feature_extraction/feature_extractor_j2.py
@@ -0,0 +1,213 @@
+"""
+Author kkappler
+
+"""
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import pdb
+import re
+
+from feature_windowing import WindowBoundaries, TRACE_WINDOW_LABELS_FOR_FEATURE_EXTRACTION
+from dcrhino3.feature_extraction.intermediate_derived_features import IntermediateFeatureDeriver
+from dcrhino3.helpers.general_helper_functions import flatten
+from dcrhino3.helpers.general_helper_functions import init_logging
+from dcrhino3.models.interval import Interval
+from dcrhino3.signal_processing.symmetric_trace import SymmetricTrace
+from dcrhino3.signal_processing.phase_rotation import rotate_phase, rotate_phase_true
+from dcrhino3.physics.util import get_expected_multiple_times
+
+from feature_extractor_j1a import calculate_boolean_features
+
+
+logger = init_logging(__name__)
+
+
+class ManualTimeWindows(object):
+    """
+    prototype, to make so changes propagate through J2, and eventually into
+    plotter and json
+
+    TODO: decide to use positive or negative for adjustments ... ??Always Add??
+    """
+    def __init__(self):
+        self.time_window = {}
+        self.time_window['primary'] = Interval(lower_bound=0.00018 - 0.001,
+                                                upper_bound=0.00018 + 0.001)
+        self.time_window['multiple_1'] = Interval(lower_bound=0.0109 - 0.001,
+                                                upper_bound=0.0109 + 0.001)
+        self.time_window['multiple_2'] = Interval(lower_bound=0.0215 - 0.00125,
+                                                upper_bound=0.0215 + 0.00125 + 0.0015)
+
+
+        self.half_widths_amplitude = {}
+        self.half_widths_amplitude['primary'] = 0.00105
+        self.half_widths_amplitude['multiple_1'] = 0.00105
+        self.half_widths_amplitude['multiple_2'] = 0.00105
+
+
+
+    def get_set_time_window(self, window_label):
+        """
+        yes, this sucks and should be done with a dict BUT it is flexible
+        for half-baked test changes ... so live with it for now
+        """
+        t_start = self.time_window[window_label].lower_bound
+        t_final = self.time_window[window_label].upper_bound
+        return t_start, t_final
+
+class FeatureExtractorJ2(object):
+    """
+    """
+    def __init__(self, component_id, trimmed_trace, transformed_args, timestamp):
+        """
+        .. todo:: window_boundaries time should just have a .to_index() method
+        .. note:: given this is run component-by-component we can simplify window_boundaries
+            to a simple dict {} rather than having 'axial', 'tangential',
+        """
+        try:
+            self.sampling_rate = transformed_args.upsample_sampling_rate
+        #pdb.set_trace()
+        except AttributeError:
+            self.sampling_rate = transformed_args.output_sampling_rate
+        self.trace = SymmetricTrace(trimmed_trace, self.sampling_rate, component_id=component_id)
+        self.transformed_args = transformed_args
+        self.manual_windows = ManualTimeWindows()
+
+
+    def extract_primary_max_time(self, window_first_time, window_final_time):
+        cond1 = self.trace.time_vector >= window_first_time
+        cond2 = self.trace.time_vector <  window_final_time
+        active_indices = np.where(cond1 & cond2)[0]
+        window_time = self.trace.time_vector[active_indices].copy()
+        window_data = self.trace.data[active_indices].copy()
+        primary_max_time_index = np.argmax(window_data)
+        primary_max_time = window_time[primary_max_time_index]
+        return primary_max_time
+
+    def extract_multiple_1_zero_crossing_time(self, window_first_time, window_final_time):
+        """
+        total bonehead way to do this - just for POC.  How to do for real:
+            probably do a LPF, then walk out from ZX until you find nearest
+            +to- polarity change.
+        Recenter to this value and sharpen (narrow) the window
+        """
+        cond1 = self.trace.time_vector >= window_first_time
+        cond2 = self.trace.time_vector <  window_final_time
+        active_indices = np.where(cond1 & cond2)[0]
+        window_time = self.trace.time_vector[active_indices].copy()
+        window_data = self.trace.data[active_indices].copy()
+
+        zero_crossing_index = np.argmin(np.abs(window_data))
+        zero_crossing_time = window_time[zero_crossing_index]
+        return zero_crossing_time
+
+    def extract_multiple_2_min_time(self, window_first_time, window_final_time):
+        """
+        yes this is a copy of extract_primary_max_time() with a -1.0 multiplier
+        """
+        cond1 = self.trace.time_vector >= window_first_time
+        cond2 = self.trace.time_vector <  window_final_time
+        active_indices = np.where(cond1 & cond2)[0]
+        window_time = self.trace.time_vector[active_indices].copy()
+        window_data = self.trace.data[active_indices].copy()
+        #pdb.set_trace()
+        window_data *= -1.0 #flip trough to a peak
+        result_time_index = np.argmax(window_data)
+        result_time = window_time[result_time_index]
+        return result_time
+
+    def extract_average_absolute_amplitude(self, time_center, window_half_width, rotate_angle=False):
+        trace_data = self.trace.data.copy()
+        if rotate_angle:
+            trace_data = rotate_phase_true(trace_data, rotate_angle)
+#            pdb.set_trace()
+#            print("truewho?")
+        window_first_time = time_center - window_half_width
+        window_final_time = time_center + window_half_width
+        cond1 = self.trace.time_vector >= window_first_time
+        cond2 = self.trace.time_vector <  window_final_time
+        active_indices = np.where(cond1 & cond2)[0]
+        window_time = self.trace.time_vector[active_indices].copy()
+        #window_data = self.trace.data[active_indices].copy()
+        window_data = trace_data[active_indices]
+        dt = window_time[1] - window_time[0]
+        window_duration = window_time[-1] - window_time[0]
+        integrated_absolute_amplitude = dt * np.sum(np.abs(window_data)) / window_duration
+        return integrated_absolute_amplitude
+
+    def extract_features(self):
+        """
+        This will be changed so that features_to_extract is taken from the json,
+        but for now will develop here;
+        """
+        extracted_features_dict = {}
+        time_features = ['primary_max_time', 'multiple_1_zero_crossing_time',
+                               'multiple_2_min_time']
+        amplitude_features = ['primary_integrated_absolute_amplitude',
+                               'multiple_1_integrated_absolute_amplitude',
+                               'multiple_2_integrated_absolute_amplitude']
+        primary_start, primary_final = self.manual_windows.get_set_time_window('primary')
+        multiple_1_start, multiple_1_final = self.manual_windows.get_set_time_window('multiple_1')
+        multiple_2_start, multiple_2_final = self.manual_windows.get_set_time_window('multiple_2')
+        #pdb.set_trace()
+        for feature_label in time_features:
+            #print(feature_label)
+            if feature_label=='primary_max_time':
+                result = self.extract_primary_max_time(primary_start, primary_final)
+            elif feature_label=='multiple_1_zero_crossing_time':
+                result = self.extract_multiple_1_zero_crossing_time(multiple_1_start, multiple_1_final)
+            elif feature_label=='multiple_2_min_time':
+                result = self.extract_multiple_2_min_time(multiple_2_start, multiple_2_final)
+            else:
+                print('logger.warning feature {} not yet supported'.format(feature_label))
+                result = None
+            output_label = '{}_{}'.format(self.trace.component_id, feature_label)
+            extracted_features_dict[output_label] = result
+
+        for feature_label in amplitude_features:
+            #print(feature_label)
+            window_label = feature_label.split('_integrated_absolute_amplitude')[0]
+            if window_label=='primary':
+                reference_label = 'primary_max_time'; rotate_angle=False;
+            elif window_label=='multiple_1':
+                reference_label = 'multiple_1_zero_crossing_time'; rotate_angle=-90.0;
+            elif window_label=='multiple_2':
+                reference_label = 'multiple_2_min_time'; rotate_angle=False;
+            else:
+                reference_label = None
+                print('bugger off!')
+                continue
+
+            window_center_time_label = '{}_{}'.format(self.trace.component_id, reference_label)
+            window_center = extracted_features_dict[window_center_time_label]
+            #pdb.set_trace()
+            result = self.extract_average_absolute_amplitude(window_center,
+                    self.manual_windows.half_widths_amplitude[window_label],
+                    rotate_angle=rotate_angle)
+
+            output_label = '{}_{}'.format(self.trace.component_id, feature_label)
+            extracted_features_dict[output_label] = result
+
+
+        delay_1_label = '{}_delay_1'.format(self.trace.component_id)
+        delay_2_label = '{}_delay_2'.format(self.trace.component_id)
+        primary_time_label = '{}_primary_max_time'.format(self.trace.component_id)
+        multiple_1_time_label = '{}_multiple_1_zero_crossing_time'.format(self.trace.component_id)
+        multiple_2_time_label = '{}_multiple_2_min_time'.format(self.trace.component_id)
+        extracted_features_dict[delay_1_label] = extracted_features_dict[multiple_1_time_label] - extracted_features_dict[primary_time_label]
+        extracted_features_dict[delay_2_label] = extracted_features_dict[multiple_2_time_label] - extracted_features_dict[multiple_1_time_label]
+#        @property
+#        def axial_delay_1(self):
+#        return self.df_dict['axial_multiple_1_max_time'] - self.df_dict['axial_primary_max_time']
+#
+#    @property
+#    def axial_delay_2(self):
+#        return self.df_dict['axial_multiple_2_max_time'] - self.df_dict['axial_multiple_1_max_time']
+        for key in extracted_features_dict.keys():
+            extracted_features_dict['J2_{}'.format(key)] = extracted_features_dict.pop('{}'.format(key))
+        return extracted_features_dict
+
+
+
+
