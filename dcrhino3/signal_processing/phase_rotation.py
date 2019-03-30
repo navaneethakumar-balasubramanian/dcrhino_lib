add correct polarity rotation

dcrhino3/signal_processing/phase_rotation.py
@@ -21,16 +21,16 @@ jj = np.complex(0.0, 1.0)
 def rotate_phase(data, phase_shift, degrees=True):
     """
     Phase rotation by fast fourier transform, multiplication by phasor, inverse fft
-    and taking reals. Rotates data by phi
-    
+    and taking reals. Rotates data by negative phi
+
     Parameters:
-        data (array): numpy array (currently 1D, need to test as 2D might need 
+        data (array): numpy array (currently 1D, need to test as 2D might need
              to transpose)
         phase shift (float): float: #of degrees to shift signal phase
-            
+
     Other Parameters:
         degrees (bool): True for degrees, false for radians
-        
+
     Returns:
         (real Numpy ndarray): Phase shifted data
     """
@@ -47,6 +47,34 @@ def rotate_phase(data, phase_shift, degrees=True):
     phase_shifted_data_time = np.real(np.fft.ifft(phase_shifted_data_fft))
     return phase_shifted_data_time
 
+def rotate_phase_true(data, phase_shift, degrees=True):
+    """
+    Phase rotation by fast fourier transform, multiplication by phasor, inverse fft
+    and taking reals. Rotates data by negative phi
+
+    Parameters:
+        data (array): numpy array (currently 1D, need to test as 2D might need
+             to transpose)
+        phase shift (float): float: #of degrees to shift signal phase
+
+    Other Parameters:
+        degrees (bool): True for degrees, false for radians
+
+    Returns:
+        (real Numpy ndarray): Phase shifted data
+    """
+    if degrees is False:
+        phase_shift_radians = phase_shift
+    else:
+        phase_shift_radians = phase_shift * np.pi / 180.0
+
+    N = len(data)
+    sign_correction = np.sign(np.fft.fftfreq(N))
+    phasor = np.exp(jj * phase_shift_radians * sign_correction)
+    fft_data = np.fft.fft(data)
+    phase_shifted_data_fft = fft_data*phasor
+    phase_shifted_data_time = np.real(np.fft.ifft(phase_shifted_data_fft))
+    return phase_shifted_data_time
 
 def determine_phase_state(data, trough_search_width):
     """
@@ -54,18 +82,18 @@ def determine_phase_state(data, trough_search_width):
     peak. Indeterminate means no troughs or too small search width, balanced means
     equal number of troughs on both sides, left_low/right_low means trough
     count imbalance.
-    
+
     Parameters:
-        data (array): numpy array (currently 1D, need to test as 2D might need 
+        data (array): numpy array (currently 1D, need to test as 2D might need
              to transpose)
-        trough-search-width (float): this is how far to the left and right of 
+        trough-search-width (float): this is how far to the left and right of
             the peak we will
             look for a minumum.  It must be large enough for the minimum to be contained,
             but not so large that we may find another minumum show up
-            
+
     Returns:
         (str): Phase state status as one of the following:
-        
+
             + 'indeterminate'
             + 'balanced'
             + 'left_low'
@@ -94,7 +122,7 @@ def determine_phase_state(data, trough_search_width):
     try:
         left_min_index = np.argmin(left_trough_region)
     except ValueError:
-        logger.warning('{} phase state'.format(phase_state))
+        #logger.warning('{} phase state'.format(phase_state))
         return phase_state
 
     left_min_value = left_trough_region[left_min_index]
@@ -122,7 +150,7 @@ def determine_phase_state(data, trough_search_width):
 
 def test_phase_rotation():
     """
-    Loads pet_trace, plots original, shift by phi, and shift by 2phi to test 
+    Loads pet_trace, plots original, shift by phi, and shift by 2phi to test
     the functionality of :func:`rotate_phase`
     """
     data = np.load('pet_trace.npy')