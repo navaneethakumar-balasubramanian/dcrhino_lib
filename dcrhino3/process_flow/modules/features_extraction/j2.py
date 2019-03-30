caa3a121ba5855d467255323e46233edb9ab0281
Karl N. Kappler <kkappler@quakefinder.com>
2019-03-29 17:53:57 -0700

    minor fix

--- dcrhino3/process_flow/modules/features_extraction/j2.py
+++ dcrhino3/process_flow/modules/features_extraction/j2.py
@@ -15,7 +15,7 @@ from dcrhino3.feature_extraction.feature_extractor_j2 import FeatureExtractorJ2
 class J2FeaturesModule(BaseFeatureModule):
     def __init__(self,json,output_path):
         BaseFeatureModule.__init__(self,json,output_path)
-        self.id = "j1"
+        self.id = "j2"
 
     def extract_feature_component(self, component_id, trace_to_process, transformed_args, timestamp):
         if component_id == 'radial':
