c1c39f13c05b5a250d3885b2e12269bfe4ed65ee
natal <natal@datacloud.com>
2019-04-01 22:28:49 +0000

    modified the way the calibration is done and there is now once central function in general_helper_functions

--- dcrhino3/acquisition/playback_raw_data.py
+++ dcrhino3/acquisition/playback_raw_data.py
@@ -8,14 +8,12 @@ import calendar
 import pdb
 import time
 from datetime import datetime
-from dcrhino.process_pipeline.trace_processing import TraceProcessing
 from dcrhino3.models.config import Config
 from dcrhino3.acquisition.config_file_utilities import extract_metadata_from_h5_file
-from dcrhino3.helpers.general_helper_functions import init_logging, interpolate_data
-import scipy as sp
+from dcrhino3.helpers.general_helper_functions import init_logging, interpolate_data, calibrate_data
 
 def main(args):
-    #pdb.set_trace()
+    # pdb.set_trace()
     save_raw = True
     save_csv = True
     save_numpy = False
@@ -54,30 +52,30 @@ def main(args):
         file_axis = [1, 2]
     else:
         file_axis = np.asarray(hf.get('axis'), dtype=np.int32)
-    #print (hf.keys())
+    # print (hf.keys())
 
-    #IDE File
-    #sensitivity = [1000000.0]
-    #file_axis = [1,2]
+    # IDE File
+    # sensitivity = [1000000.0]
+    # file_axis = [1,2]
 
-    #Rhino File
-    #sensitivity = [2.5,2.5,2.5]#500g
-    #sensitivity = [12.5,12.5,12.5]#500g
-    #file_axis = [2,1]
+    # Rhino File
+    # sensitivity = [2.5,2.5,2.5]#500g
+    # sensitivity = [12.5,12.5,12.5]#500g
+    # file_axis = [2,1]
 
     print(sensitivity)
     print(file_axis)
     if start_time == 0:
         start_time = ts[0]
     else:
-        start_time = ts[ts>=start_time][0]
+        start_time = ts[ts >= start_time][0]
 
     if end_time == 0:
         end_time = int(ts[-1])+1
 
-    #pdb.set_trace()
+    # pdb.set_trace()
 
-    seq = np.asarray(hf.get('seq'), dtype=np.uint32)
+    # seq = np.asarray(hf.get('seq'), dtype=np.uint32)
     tx_sequence = np.asarray(hf.get('cticks'), dtype=np.uint32)
 
 
@@ -108,11 +106,10 @@ def main(args):
     accelerometer_max_voltage = float(global_config.accelerometer_max_voltage)
     rhino_version = global_config.rhino_version
 
-    if len(sensitivity)>1:
+    if len(sensitivity) > 1:
         is_ide_file = False
     else:
         is_ide_file = True
-    trace_processor = TraceProcessing(global_config, is_ide_file,accelerometer_max_voltage,rhino_version)
 
     if is_ide_file:
         # x_sensitivity = sensitivity[0]
@@ -129,30 +126,32 @@ def main(args):
         # y_data = ((accelerometer_max_voltage/2.0)-(np.asarray(hf.get('y'),dtype=np.int32)*5.0/65535))/(x_sensitivity/1000.0)#/1e6
         # z_data = ((accelerometer_max_voltage/2.0)-(np.asarray(hf.get('z'),dtype=np.int32)*5.0/65535))/(x_sensitivity/1000.0)#/1e6
         print("IDE file")
-        x_data = trace_processor._apply_calibration(np.asarray(hf.get('x'),dtype=np.float32),sensitivity[0])
-        y_data = trace_processor._apply_calibration(np.asarray(hf.get('y'),dtype=np.float32),sensitivity[1])
-        z_data = trace_processor._apply_calibration(np.asarray(hf.get('z'),dtype=np.float32),sensitivity[2])
+        x_data = calibrate_data(np.asarray(hf.get('x'), dtype=np.float32), sensitivity[0], is_ide_file)
+        y_data = calibrate_data(np.asarray(hf.get('y'), dtype=np.float32), sensitivity[1], is_ide_file)
+        z_data = calibrate_data(np.asarray(hf.get('z'), dtype=np.float32), sensitivity[2], is_ide_file)
     else:
-        # x_data = np.asarray(hf.get('x'),dtype=np.float32)#/sensitivity[0]
-        # y_data = np.asarray(hf.get('y'),dtype=np.float32)#/sensitivity[0]
-        # z_data = np.asarray(hf.get('z'),dtype=np.float32)#/sensitivity[0]
         print("Rhino file")
-        x_data = trace_processor._apply_calibration(np.asarray(hf.get('x'),dtype=np.uint32),sensitivity[0])
-        y_data = trace_processor._apply_calibration(np.asarray(hf.get('y'),dtype=np.uint32),sensitivity[0])
-        z_data = trace_processor._apply_calibration(np.asarray(hf.get('z'),dtype=np.uint32),sensitivity[0])
+        x_data = calibrate_data(np.asarray(hf.get('x'), dtype=np.uint32), sensitivity[0], accelerometer_max_voltage,
+                                rhino_version)
+        y_data = calibrate_data(np.asarray(hf.get('y'), dtype=np.uint32), sensitivity[1], accelerometer_max_voltage,
+                                rhino_version)
+        z_data = calibrate_data(np.asarray(hf.get('z'), dtype=np.uint32), sensitivity[2], accelerometer_max_voltage,
+                                rhino_version)
 
     # pdb.set_trace()
     output_path = os.path.dirname(fname)
 
     if save_raw:
         print("Saving Raw Data")
-        cols = ["raw_ts","adc_x","adc_y","adc_z","cal_x","cal_y","cal_z","tx_sequence"]
-        d = {cols[0]:ts,cols[1]:np.asarray(hf.get('x'),dtype=np.float32),cols[2]:np.asarray(hf.get('y'),dtype=np.float32),cols[3]:np.asarray(hf.get('z'),dtype=np.float32),
-            cols[4]:x_data,cols[5]:y_data,cols[6]:z_data, cols[7]:tx_sequence}
+        cols = ["raw_ts", "adc_x", "adc_y", "adc_z", "cal_x", "cal_y", "cal_z", "tx_sequence"]
+        d = {cols[0]: ts, cols[1]: np.asarray(hf.get('x'), dtype=np.float32), cols[2]: np.asarray(hf.get('y'),
+                                                                                                  dtype=np.float32),
+             cols[3]: np.asarray(hf.get('z'), dtype=np.float32), cols[4]: x_data, cols[5]: y_data, cols[6]: z_data,
+             cols[7]: tx_sequence}
         output_data = pd.DataFrame(data=d)
         output_data = output_data[cols]
-        csv_path = os.path.join(output_path,original_name+"_raw.csv".format(output_sampling_rate))
-        output_data.to_csv(csv_path,index=False)
+        csv_path = os.path.join(output_path, original_name + "_raw.csv".format(output_sampling_rate))
+        output_data.to_csv(csv_path, index=False)
 
     hf.close()
 
@@ -161,15 +160,15 @@ def main(args):
     ideal_timestamps = (np.arange(total_seconds*output_sampling_rate)*1.0/output_sampling_rate)+start_time
     ideal_timestamps = ideal_timestamps[ideal_timestamps <= end_time]
     # ideal_timestamps = (np.arange(total_seconds * output_sampling_rate) * 1.0 / output_sampling_rate) + start_time
-    index = (ts>=start_time) & (ts<end_time)
+    index = (ts >= start_time) & (ts < end_time)
     digitizer_timestamps = ts[index]
 
 
 
-    time_filtered_data = []
-    time_filtered_data.append( x_data[index])
-    time_filtered_data.append( y_data[index])
-    time_filtered_data.append( z_data[index])
+    time_filtered_data = list()
+    time_filtered_data.append(x_data[index])
+    time_filtered_data.append(y_data[index])
+    time_filtered_data.append(z_data[index])
     #time_filtered_x_raw_data=time_filtered_x_raw_data[time_filtered_x_raw_data < end_time]
 
 
@@ -192,9 +191,13 @@ def main(args):
     tangential_axis_index = file_axis[1]-1
     radial_axis_index = 3-axial_axis_index-tangential_axis_index
 
-    axial_path = os.path.join(output_path,"axial",output_name+"_axial_{}_{}.npy".format(axis[axial_axis_index],output_sampling_rate))
-    tangential_path = os.path.join(output_path,"tangential",output_name+"_tangential_{}_{}.npy".format(axis[tangential_axis_index],output_sampling_rate))
-    radial_path = os.path.join(output_path,"radial",output_name+"_radial_{}_{}.npy".format(axis[radial_axis_index],output_sampling_rate))
+    axial_path = os.path.join(output_path, "axial", output_name+"_axial_{}_{}.npy".format(axis[axial_axis_index],
+                                                                                         output_sampling_rate))
+    tangential_path = os.path.join(output_path, "tangential",
+                                   output_name+"_tangential_{}_{}.npy".format(axis[tangential_axis_index],
+                                                                              output_sampling_rate))
+    radial_path = os.path.join(output_path, "radial", output_name+"_radial_{}_{}.npy".format(axis[radial_axis_index],
+                                                                                            output_sampling_rate))
 
 
 
@@ -203,9 +206,9 @@ def main(args):
     #time_filtered_interp_y_data = interpolate_data(ideal_timestamps,digitizer_timestamps,time_filtered_y_raw_data)
     #time_filtered_interp_z_data = interpolate_data(ideal_timestamps,digitizer_timestamps,time_filtered_z_raw_data)
     print("interpolating")
-    interp_data.append(interpolate_data(digitizer_timestamps,time_filtered_data[0],ideal_timestamps))#X data
-    interp_data.append(interpolate_data(digitizer_timestamps,time_filtered_data[1],ideal_timestamps))#Y data
-    interp_data.append(interpolate_data(digitizer_timestamps,time_filtered_data[2],ideal_timestamps))#Z data
+    interp_data.append(interpolate_data(digitizer_timestamps, time_filtered_data[0], ideal_timestamps))#X data
+    interp_data.append(interpolate_data(digitizer_timestamps, time_filtered_data[1], ideal_timestamps))#Y data
+    interp_data.append(interpolate_data(digitizer_timestamps, time_filtered_data[2], ideal_timestamps))#Z data
 
     if save_numpy:
         print("Saving Files")
@@ -215,24 +218,27 @@ def main(args):
             os.mkdir(os.path.dirname(tangential_path))
         if not os.path.exists(os.path.dirname(radial_path)):
             os.mkdir(os.path.dirname(radial_path))
-        np.save(axial_path,interp_data[file_axis[0]-1])
-        np.save(tangential_path,interp_data[file_axis[1]-1])
-        np.save(radial_path,interp_data[5-file_axis[0]-file_axis[1]])
+        np.save(axial_path, interp_data[file_axis[0]-1])
+        np.save(tangential_path, interp_data[file_axis[1]-1])
+        np.save(radial_path, interp_data[5-file_axis[0]-file_axis[1]])
 
 
     if save_csv:
         print("Creating CSV File")
 
-        datetimes = [datetime.strftime(datetime.utcfromtimestamp(x),"%Y-%m-%d %H:%M:%S.%f") for x in ideal_timestamps]
-        cols = ["Ideal_Timestamps","DateTimes","Axial_{}".format(axis[axial_axis_index]),"Tangential_{}".format(axis[tangential_axis_index]),"Radial_{}".format(axis[radial_axis_index])]
-        d = {cols[0]:ideal_timestamps,cols[1]:datetimes,cols[2]:interp_data[axial_axis_index],cols[3]:interp_data[tangential_axis_index],cols[4]:interp_data[radial_axis_index]}
+        datetimes = [datetime.strftime(datetime.utcfromtimestamp(x), "%Y-%m-%d %H:%M:%S.%f") for x in ideal_timestamps]
+        cols = ["Ideal_Timestamps", "DateTimes", "Axial_{}".format(axis[axial_axis_index]), "Tangential_{}".format(
+            axis[tangential_axis_index]), "Radial_{}".format(axis[radial_axis_index])]
+        d = {cols[0]: ideal_timestamps, cols[1]: datetimes, cols[2]: interp_data[axial_axis_index],
+             cols[3]: interp_data[tangential_axis_index], cols[4]: interp_data[radial_axis_index]}
         output_data = pd.DataFrame(data=d)
         output_data = output_data[cols]
-        csv_path = os.path.join(output_path,original_name+"_quadratic_interpolated_{}.csv".format(output_sampling_rate))
-        output_data.to_csv(csv_path,index=False)
+        csv_path = os.path.join(output_path, original_name+"_quadratic_interpolated_{}.csv".format(
+            output_sampling_rate))
+        output_data.to_csv(csv_path, index=False)
 
     t1 = datetime.now()
-    fig = plt.figure("DataCloud Rhino Raw Data Playback",figsize=(10,5))
+    fig = plt.figure("DataCloud Rhino Raw Data Playback", figsize=(10, 5))
     plt.subplots_adjust(hspace=0.5)
     # ax.ticklabel_format(useOffset=False, style='plain')
     #plt.figure(1)
@@ -252,29 +258,29 @@ def main(args):
     #df.to_csv("./data_for_jim.csv", sep=',',index=False)
     ########################################################################
 
-    axis_font = {'fontname':'Arial', 'size':'8'}
+    axis_font = {'fontname': 'Arial', 'size': '8'}
 
-    axial_plot = plt.subplot2grid((3, 1), (0, 0),colspan=1)
+    axial_plot = plt.subplot2grid((3, 1), (0, 0), colspan=1)
     axial_plot.ticklabel_format(useOffset=False, style='plain')
     axial_plot.set_title("Axial Component", **axis_font)
     # axial_plot.set_xlabel("Time (UTC)", **axis_font)
     axial_plot.set_ylabel("g", **axis_font)
     axial_plot.tick_params(axis='x',          # changes apply to the x-axis
-                            which='both',      # both major and minor ticks are affected
-                            top=False,         # ticks along the top edge are off
-                            labelbottom=False)
+                           which='both',      # both major and minor ticks are affected
+                           top=False,         # ticks along the top edge are off
+                           labelbottom=False)
 
-    tangential_plot = plt.subplot2grid((3, 1), (1, 0),colspan=1,sharex=axial_plot)
+    tangential_plot = plt.subplot2grid((3, 1), (1, 0), colspan=1, sharex=axial_plot)
     # tangential_plot.ticklabel_format(useOffset=False, style='plain')
-    tangential_plot.set_title("Tangential Component",**axis_font)
+    tangential_plot.set_title("Tangential Component", **axis_font)
     # tangential_plot.set_xlabel("Time (UTC)", **axis_font)
     tangential_plot.set_ylabel("g", **axis_font)
     tangential_plot.tick_params(axis='x',          # changes apply to the x-axis
-                            which='both',      # both major and minor ticks are affected
-                            top=False,         # ticks along the top edge are off
-                            labelbottom=False)
+                                which='both',      # both major and minor ticks are affected
+                                top=False,         # ticks along the top edge are off
+                                labelbottom=False)
 
-    radial_plot = plt.subplot2grid((3, 1), (2, 0),colspan=1,sharex=axial_plot)
+    radial_plot = plt.subplot2grid((3, 1), (2, 0), colspan=1, sharex=axial_plot)
     radial_plot.set_title("Radial Component", **axis_font)
     radial_plot.set_xlabel("Time (UTC)", **axis_font)
     radial_plot.set_ylabel("g", **axis_font)
@@ -282,17 +288,17 @@ def main(args):
     # plt.plot([datetime.utcfromtimestamp(x) for x in digitizer_timestamps],time_filtered_data[file_axis[0]-1],'k')
     # plt.plot([datetime.utcfromtimestamp(x) for x in ideal_timestamps],interp_data[file_axis[0]-1],'r')
 
-    axial_plot.plot([datetime.utcfromtimestamp(x) for x in ideal_timestamps],interp_data[axial_axis_index], 'r',
-                    marker="X")
+    axial_plot.plot([datetime.utcfromtimestamp(x) for x in ideal_timestamps], interp_data[axial_axis_index], 'r')
     # axial_plot.plot([datetime.utcfromtimestamp(x) for x in digitizer_timestamps],time_filtered_data[
     #     axial_axis_index],'b',marker=".")
-    tangential_plot.plot([datetime.utcfromtimestamp(x) for x in ideal_timestamps],interp_data[tangential_axis_index],'b')
-    radial_plot.plot([datetime.utcfromtimestamp(x) for x in ideal_timestamps],interp_data[radial_axis_index],'g')
+    tangential_plot.plot([datetime.utcfromtimestamp(x) for x in ideal_timestamps], interp_data[
+        tangential_axis_index], 'b')
+    radial_plot.plot([datetime.utcfromtimestamp(x) for x in ideal_timestamps], interp_data[radial_axis_index], 'g')
 
-    plt.suptitle("Raw Data Playback of File {} at {} Hz".format(os.path.basename(fname),output_sampling_rate))
+    plt.suptitle("Raw Data Playback of File {} at {} Hz".format(os.path.basename(fname), output_sampling_rate))
 
     # plt.tight_layout()
-    fig.savefig(fname.replace(".h5",".png"))
+    fig.savefig(fname.replace(".h5", ".png"))
     if args.plot:
         plt.show()
 
@@ -304,11 +310,13 @@ def main(args):
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Playback Raw Data -  Copyright (c) 2018 DataCloud")
-    argparser.add_argument('-st', '--start_time', help="Start timestamp to process 'YYYY-MM-DD HH:MM:SS' in UTC Time", default=None)
-    argparser.add_argument('-et', '--end_time', help="End timestamp to process 'YYYY-MM-DD HH:MM:SS' in UTC Time", default=None)
+    argparser.add_argument('-st', '--start_time', help="Start timestamp to process 'YYYY-MM-DD HH:MM:SS' in UTC Time",
+                           default=None)
+    argparser.add_argument('-et', '--end_time', help="End timestamp to process 'YYYY-MM-DD HH:MM:SS' in UTC Time",
+                           default=None)
     argparser.add_argument('-sr', '--sampling_rate', help="Resampling Rate for Output Data", default=None)
-    argparser.add_argument('-source','--source_path',help="Path to source file",default=None)
-    argparser.add_argument('-plot','--plot',help="Show plot for axial axis",default=False)
+    argparser.add_argument('-source', '--source_path', help="Path to source file", default=None)
+    argparser.add_argument('-plot', '--plot', help="Show plot for axial axis", default=False)
 
     args = argparser.parse_args()
 
