4b4acd09ec209c240527b97bedfa174959e8ab6d
natal <natal@datacloud.com>
2019-04-02 18:59:09 +0000

    Will create segy with the first config file if there are more than one config file detected

--- dcrhino3/process_flow/modules/trace_processing/export_segy.py
+++ dcrhino3/process_flow/modules/trace_processing/export_segy.py
@@ -36,23 +36,23 @@ class ExportSEGYModule(BaseTraceModule):
         # output_df = trace.dataframe.copy()
 
         if len(np.unique(trace.dataframe['acorr_file_id'])) > 1:
-            m = "More than one config file identified while generating SEGY"
+            m = "More than one config file identified while generating SEGY.  Using first config file"
             logger.warning(m)
             print(m)
         else:
             m = "Only one config file identified while generating SEGY"
             logger.info(m)
-            basename = os.path.basename(self.output_file_basepath(extension=""))
-            output_path = os.path.join(os.path.dirname(self.output_file_basepath(extension=".h5")),
-                                       "{}_{}.sgy".format(basename, self.generate_output_name(trace.applied_modules)))
-            self.global_config = trace.global_config_by_index(trace.dataframe['acorr_file_id'].values[0])
-            self.sampling_rate = self.get_sampling_rate(trace.applied_modules)
-            self.stream.stats = AttribDict()
-            self.stream.stats.textual_file_header = self.generate_textual_header()
-            self.stream.stats.binary_file_header = SEGYBinaryFileHeader()
-            trace.dataframe.apply(self.save_as_segy, axis=1)
-            self.stream.write(output_path, format="SEGY", data_encoding=1, byteorder=">",
-                              textual_header_encoding="ASCII")
+        basename = os.path.basename(self.output_file_basepath(extension=""))
+        output_path = os.path.join(os.path.dirname(self.output_file_basepath(extension=".h5")),
+                                   "{}_{}.sgy".format(basename, self.generate_output_name(trace.applied_modules)))
+        self.global_config = trace.global_config_by_index(trace.dataframe['acorr_file_id'].values[0])
+        self.sampling_rate = self.get_sampling_rate(trace.applied_modules)
+        self.stream.stats = AttribDict()
+        self.stream.stats.textual_file_header = self.generate_textual_header()
+        self.stream.stats.binary_file_header = SEGYBinaryFileHeader()
+        trace.dataframe.apply(self.save_as_segy, axis=1)
+        self.stream.write(output_path, format="SEGY", data_encoding=1, byteorder=">",
+                          textual_header_encoding="ASCII")
         return trace
 
     def generate_output_name(self, applied_modules_list):
