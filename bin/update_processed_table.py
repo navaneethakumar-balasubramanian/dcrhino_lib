1919ca0a2dd5ed982ba63b9a1aaab0b3f0ce0827
Thiago Mourao <xtecox@gmail.com>
2019-04-25 19:26:17 -0300

    a

--- bin/update_processed_table.py
+++ bin/update_processed_table.py
@@ -28,12 +28,14 @@ def update_processed_table(mine_name,env_config_path):
     for i,file in enumerate(files):
         complete_path_folder = file.replace(file_to_look_for, "")
         relative_path_folder = complete_path_folder.replace(processed_folder_path,"")
+        processed_h5_path = os.path.join(complete_path_folder,"processed.h5")
+        process_flow_path = os.path.join(complete_path_folder,"process_flow.json")
 
-        if len(processed_holes) == 0 or relative_path_folder not in processed_holes.output_folder_name.values:
+        if os._exists(processed_h5_path) and os._exists(process_flow_path) and len(processed_holes) == 0 or relative_path_folder not in processed_holes.output_folder_name.values:
             logger.info("Processing " +str(i+1) + "/"+str(len(files)) + " : "+ complete_path_folder)
             trace_data = TraceData()
-            trace_data.load_from_h5(os.path.join(complete_path_folder,"processed.h5"))
-            with open(os.path.join(complete_path_folder,"process_flow.json"), 'r') as f:
+            trace_data.load_from_h5(processed_h5_path)
+            with open(os.path.join(process_flow_path), 'r') as f:
                 process_json = json.load(f)
 
             id = process_json['id']
@@ -44,6 +46,7 @@ def update_processed_table(mine_name,env_config_path):
             else:
                 date_of_process_seconds = 0
             db_helper.save_processed_trace(trace_data, id, json.dumps(process_json),relative_path_folder, date_of_process_seconds, 99999)
+
         else:
             logger.info("Ignored " +str(i+1) +"/"+ str(len(files)) + " : " + complete_path_folder )
 
