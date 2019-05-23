441ca3ea608af7add0df76dac8a62ce8e70bb462
Thiago Mourao <xtecox@gmail.com>
2019-05-23 10:45:26 -0300

    Sanity check plotter

--- bin/mwd_check.py
+++ bin/mwd_check.py
@@ -1,10 +1,29 @@
 from dcrhino3.models.env_config import EnvConfig
 from dcrhino3.helpers.rhino_db_helper import RhinoDBHelper
 from dcrhino3.helpers.mwd_helper import MWDHelper
-from mwd_check_plotter import make_plot
+
 import matplotlib.pyplot as plt
 import argparse
+import numpy as np
+import pandas as pd
+import datetime
 
+
+def sanity_plot(mwd_df, traces_df):
+    time_shift_applied_to_mwd = 0
+    mwd_df.start_time = pd.to_datetime(mwd_df.start_time)
+    shifted_time = mwd_df.start_time + datetime.timedelta(seconds=time_shift_applied_to_mwd)
+    traces_df['datetime'] = pd.to_datetime(traces_df['timestamp'], unit='s')
+    traces_df.max_axial_acceleration = np.clip(traces_df.max_axial_acceleration, 0, 9.76)
+    # dft = dft[dft.acorr_file_id==34]    #plt.plot(mwd_df.start_time_old, mwd_df.depth, '*', label ='mwd original');
+    plt.plot(shifted_time, mwd_df.depth, '*', label='mwd shifted');
+    plt.plot(traces_df.datetime, traces_df.max_axial_acceleration, '*', label='rhino_traces');
+    plt.legend()
+    ttl_string = "Time shift {}s applied to mwd".format(time_shift_applied_to_mwd)
+    plt.title(ttl_string)
+    plt.xlabel('Time/Date')
+    plt.ylabel('Depth (m)')
+    plt.show()
 
 def check(mine_name):
     env_config = EnvConfig()
@@ -12,34 +31,22 @@ def check(mine_name):
     mwd_df = mwd_helper.get_rhino_mwd_from_mine_name(mine_name)
     conn = env_config.get_rhino_db_connection_from_mine_name(mine_name)
 
-    #sqlconn = env_config.get_rhino_sql_connection_from_mine_name('mont_wright')
-
     db_helper= RhinoDBHelper(conn=conn)
     result = db_helper.client.execute("select acorr_file_id , timestamp, max_axial_acceleration from acorr_traces order by timestamp")
     df_traces = db_helper.query_result_to_pd(result,['acorr_file_id','timestamp','max_axial_acceleration'])
-    accel = df_traces.max_axial_acceleration
 
-    timestamps_mwd = mwd_df.start_time
-    depth_mwd = mwd_df.depth.values
-    timestamps = df_traces
+    sanity_plot(mwd_df,df_traces)
 
-    make_plot(mwd_df, df_traces)
 
-    plt.show()
 
 if __name__ == '__main__':
-    use_argparse = True#False
-    if use_argparse:
-        argparser = argparse.ArgumentParser(description="Copyright (c) 2018 DataCloud")
+    argparser = argparse.ArgumentParser(description="Copyright (c) 2018 DataCloud")
 
-        argparser.add_argument("mine_name", type=str,
-        help="Name of mine to check, matching env_config.json" )
+    argparser.add_argument("mine_name", type=str,
+    help="Name of mine to check, matching env_config.json" )
 
-        args = argparser.parse_args()
-        mine_name = args.mine_name
-
-    else:
-        mine_name = 'bma'
+    args = argparser.parse_args()
+    mine_name = args.mine_name
 
     check(mine_name)
 
--- dcrhino3/helpers/db/sqls/create_sensor_files.sql
+++ dcrhino3/helpers/db/sqls/create_sensor_files.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `sensor_files` (
-	`raw_file_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`sensor_file_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`file_path` TEXT NOT NULL COLLATE 'utf8_bin',
 	`rig_id` VARCHAR(90) NOT NULL COLLATE 'utf8_bin',
 	`sensor_id` VARCHAR(90) NOT NULL COLLATE 'utf8_bin',
@@ -7,8 +7,15 @@ CREATE TABLE IF NOT EXISTS `sensor_files` (
 	`min_ts` INT(11) NOT NULL,
 	`max_ts` INT(11) NOT NULL,
 	`config_str` TEXT NOT NULL COLLATE 'utf8_bin',
+	`archived` TINYINT(4) NULL DEFAULT '0',
 	`type` INT(11) UNSIGNED NOT NULL,
-	PRIMARY KEY (`raw_file_id`)
+	PRIMARY KEY (`sensor_file_id`),
+	INDEX `min_ts` (`min_ts`),
+	INDEX `max_ts` (`max_ts`),
+	INDEX `rig_id` (`rig_id`),
+	INDEX `sensor_id` (`sensor_id`),
+	INDEX `digitizer_id` (`digitizer_id`),
+	INDEX `archived` (`archived`)
 )
 COLLATE='utf8_bin'
 ENGINE=InnoDB
