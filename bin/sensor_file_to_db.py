3ffcb3de7d30eb1cad69e218eda0bd82ed2c2083
Thiago Mourao <xtecox@gmail.com>
2019-05-24 18:28:14 -0300

    save

--- bin/sensor_file_to_db.py
+++ bin/sensor_file_to_db.py
@@ -53,8 +53,8 @@ def raw_trace_h5_to_db(h5_file_path,env_config,min_ts,max_ts,chunk_size=500):
     if file_exists:
         logger.warning("IGNORED THIS FILE: DUPLICATED")
         return
-    #else:
-    #    file_id = sql_db_helper.sensor_files.add(h5_file_path,global_config.rig_id,str(global_config.sensor_serial_number),str(global_config.digitizer_serial_number),int(min),int(max),json.dumps(vars(global_config), indent=4),1,status='valid')
+    else:
+        file_id = sql_db_helper.sensor_files.add(h5_file_path,global_config.rig_id,str(global_config.sensor_serial_number),str(global_config.digitizer_serial_number),int(min),int(max),json.dumps(vars(global_config), indent=4),1,status='valid')
 
     return
     list_df = splitDataFrameIntoSmaller(l1h5_dataframe.reset_index(drop=True),chunk_size)
