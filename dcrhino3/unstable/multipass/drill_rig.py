d9dac47085975d53c6c32a50635f0846a49d1223
Karl N. Kappler <kkappler@quakefinder.com>
2019-05-31 15:53:57 -0700

    towards multipass logics

---
+++ dcrhino3/unstable/multipass/drill_rig.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 16:36:45 2019
+
+@author: kkappler
+
+    @property
+    def installed_resonant_length(self):
+        #this is the distance from the bottom of the shocksub to the bottom of the bit
+        return self.sensor_distance_to_shocksub + self.sensor_distance_to_source
+
+"""
+
+from __future__ import absolute_import, division, print_function
+
+
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pdb
+
+from dcrhino3.models.trace_dataframe import TraceData
+
+from dcrhino3.unstable.multipass.drill_string_component import DrillStringComponent
+from dcrhino3.unstable.multipass.drill_string_component import DRILL_STRING_COMPONENT_TYPES
+
+NUM_DRILL_STRING_COMPONENTS_SUPPORTED = 10
+
+class DrillRig(object):
+    """
+    20190529: this is needed to unkerfunk the metadata and gui stuff
+    THis class is intended to be initialized with the DIRECT field data info
+    N.B. THis can be from client .pdf in the case of some drill string component
+    measurements or from manual measurements e.g. in the case of sensor position
+
+    Note this is not strictly speaking a drill-rig, but a drill-rig-rhino combination
+    as sensor position is factored in here.  In a later version consider factoring
+    DrillRig and DeployedRhinoOnDrillRig as separate entitites.  In the meantime
+    we can think of this class as an InstrumentedDrillRig()
+
+
+    """
+    def __init__(self, field_config=None):
+        self.drill_string_components = []
+        self.field_config = field_config
+        self.installed_steels = []
+        self.variable_steels = []
+        self.bit = None
+        self.collar = []
+        self.saver_sub = []
+        self.shock_sub = None
+        self.rotary_bit_sub = None
+        self.sensor_position = None
+        if self.field_config is not None:
+            self.populate_drill_string_components()
+
+    def populate_drill_string_components(self):
+        """
+        .. :WARNING: If this gets called multiple times it can augment the lists
+        making it seem there are too may components ...try not to call this method
+        but rather init with the field config.
+        @Natal: should we be checking installed vs not installed on components other
+        than steels?
+        """
+        if self.field_config is None:
+            logger.warning("cannot populate drill string compoennts, field config is None")
+            return
+        field_config =self.field_config
+        total_drill_string_length = 0.0
+        for i_drill_string_component in range(1, NUM_DRILL_STRING_COMPONENTS_SUPPORTED+1):
+            attribute_label = 'drill_string_component{}'.format(i_drill_string_component)
+            attribute = field_config.__getattribute__(attribute_label)
+            attributes_list = attribute.split(',')
+            dsc = DrillStringComponent(attributes_list=attributes_list)
+            total_drill_string_length += dsc.length_in_meters
+            if dsc.component_type == 'steel':
+                if dsc.installation == 'installed':
+                    self.installed_steels.append(dsc)
+                if dsc.installation == 'variable':
+                    self.variable_steels.append(dsc)
+            elif dsc.component_type == 'collar':
+                self.collar.append(dsc)
+            elif dsc.component_type == 'saver_sub':
+                self.saver_sub.append(dsc)
+            elif dsc.component_type == 'bit':
+                self.bit = dsc
+            elif dsc.component_type == 'shock_sub':
+                self.shock_sub = dsc
+            elif dsc.component_type == 'rotary_bit_sub':
+                self.rotary_bit_sub = dsc
+
+        #calculate_and_assign_some lengths
+        self.sensor_position = field_config.sensor_position
+        self.total_drill_string_length = total_drill_string_length
+        self.total_installed_length = self.total_drill_string_length - sum(self.variable_steels_lengths)
+        return
+
+#    def calculate_and_assign_some lengths(self)
+#        self.sensor_position = field_config.sensor_position
+#        self.total_drill_string_length = total_drill_string_length
+#        self.total_installed_length =
+#        print(total_drill_string_length)
+#        return
+
+
+    @property
+    def installed_steels_length(self):
+        """
+        ..:todo: this could be made a method of the global_config class
+        loop over all drill string components to check for elements of
+        type steel (3) and installed (1) to get the default length
+        """
+        installed_steels_length = 0.0
+        for dsc in self.installed_steels:#
+            installed_steels_length += dsc.length_in_meters
+        return installed_steels_length
+
+    @property
+    def variable_steels_lengths(self):
+        variable_steels_lengths = [dsc.length_in_meters for dsc in self.variable_steels]
+        return variable_steels_lengths
+
+    def total_drill_string_length(self, variable=False):
+        for i_drill_string_component in range(1, NUM_DRILL_STRING_COMPONENTS_SUPPORTED+1):
+            return
+
+    @property
+    def sensor_distance_to_shocksub(self):
+        """
+        how do we handle when SS DNE?
+        """
+        self.sensor_position.value_in_meters() - self.shocksub_length
+
+    @property
+    def installed_resonant_length(self):
+        """
+        this is the distance from the bottom of the shocksub to the bottom of the bit
+        from config.py it is calculated as:
+        #return self.sensor_distance_to_shocksub + self.sensor_distance_to_source
+        But what does that mean when we have no shocksub.  We need clearer descriptions here.
+        """
+        return self.field_config.installed_resonant_length - sum(self.variable_steels_lengths)
+
+#    #<HACK FOR LINE CREEK>
+#    logger.critical("HACK ALERT!!! If you are seeing this message it means\
+#                    this is NOT production code")
+#    print("HACK LINE CREEK CREEK CREEK")
+#    #variable_steels_lengths = [15.24, 15.24]
+#    #</HACK FOR LINE CREEK>
+#    return variable_steels_lengths
+
+
+
+#class DeployedDrill(DrillRig):
+#    def __init__(self, field_config):
+#        DrillRig.__init__(self, field_config)
+#        self.sensor_position = None
+
+
+def test(acorr_filename=None):
+    """
+    """
+    print('hi')
+
+def main():
+    """
+    """
+    test()
+    print("finito {}".format(datetime.datetime.now()))
+
+if __name__ == "__main__":
+    main()
--- dcrhino3/unstable/multipass/drill_string_component.py
+++ dcrhino3/unstable/multipass/drill_string_component.py
@@ -31,10 +31,73 @@ from dcrhino3.models.trace_dataframe import TraceData
 from dcrhino3.helpers.general_helper_functions import init_logging
 from dcrhino3.models.metadata import Measurement
 
-#from dcrhino3.unstable.multipass.drill_string_component import DrillStringComponent
-from dcrhino3.unstable.multipass.drill_rig import DrillRig
-
 logger = init_logging(__name__)
+
+"""
+@Natal: why do we need these integers codes? is this a gui thing?
+-
+"""
+DRILL_STRING_COMPONENT_TYPES = {}
+DRILL_STRING_COMPONENT_TYPES[1] = 'bit'
+DRILL_STRING_COMPONENT_TYPES[2] = 'collar'
+DRILL_STRING_COMPONENT_TYPES[3] = 'steel'
+DRILL_STRING_COMPONENT_TYPES[4] = 'saver sub'
+DRILL_STRING_COMPONENT_TYPES[5] = 'shock sub'
+DRILL_STRING_COMPONENT_TYPES[6] = 'other'
+DRILL_STRING_COMPONENT_TYPES[7] = 'rotary bit sub'
+
+DRILL_STRING_COMPONENT_INSTALLATIONS = {}
+DRILL_STRING_COMPONENT_INSTALLATIONS[-1] = 'not installed'
+DRILL_STRING_COMPONENT_INSTALLATIONS[0] = 'variable'
+DRILL_STRING_COMPONENT_INSTALLATIONS[1] = 'installed'
+
+LENGTH_UNITS = {}
+LENGTH_UNITS[1] = 'ft'
+LENGTH_UNITS[2] = 'in'
+LENGTH_UNITS[3] = 'm'
+LENGTH_UNITS[4] = 'cm'
+LENGTH_UNITS[5] = 'mm'
+
+NUM_DRILL_STRING_COMPONENTS_SUPPORTED = 10
+
+class DrillStringComponent(object):
+    """
+    ..:warning: there is possibility for error here as the length_in_meters
+    applies a roundoff on each component, the roundoff should happen after summation.
+    """
+    def __init__(self, attributes_list=None):
+        self._component_type = None
+        self._installation = None
+        self._length = None
+        self._length_units = None
+        self._outer_diameter = None
+        self._outer_diameter_units = None
+        if attributes_list is not None:
+            if len(attributes_list) != 6:
+                logger.error('expected six values in drill string component attributes, got {}'.format(len(attributes_list)))
+                raise Exception
+            self._component_type = int(attributes_list[0])
+            self._installation = int(attributes_list[1])
+            self._length= float(attributes_list[2])
+            self._length_units= int(attributes_list[3])
+            self._outer_diameter = float(attributes_list[4])
+            self._outer_diameter_units = int(attributes_list[5])
+
+    @property
+    def component_type(self):
+        return DRILL_STRING_COMPONENT_TYPES[self._component_type]
+    @property
+    def installation(self):
+        return DRILL_STRING_COMPONENT_INSTALLATIONS[self._installation]
+    @property
+    def length_units(self):
+        return LENGTH_UNITS[self._length_units]
+    @property
+    def length_in_meters(self):
+        measurement = Measurement((self._length, self._length_units))
+        return measurement.value_in_meters()
+
+
 
 
 
@@ -248,76 +311,6 @@ def nearest_time_to_transition_depth(dataframe,
     return closest_drill_stop_times, closest_drill_stop_depths
 
 
-def update_acorr_with_resonance_info(acorr_trace, transition_depth_offset_m=-1.0):
-    """
-    acorr_trace is of type dcrhino3.models.trace_dataframe.TraceData()
-
-    Conceptual flow:
-        1. find the approximate depths at which we would expect a new steel to
-        be attached (transition_depths)
-        2. Determine which transition depths are surpassed significantly.
-        i.e. does the bit go significantly deeper than the transition depth (>~2m)
-        -- Now you have a list of relevant transition depths for the given blasthole-acorr
-        3. Place the 'actual' transition depth at the center of the drill stop
-        nearest to the theoretical transition depth
-    """
-    global_config = acorr_trace.first_global_config
-    drill_rig = DrillRig(field_config=acorr_trace.first_global_config)
-    pdb.set_trace()
-    installed_steels_length = drill_rig.installed_steels_length
-    variable_steels_lengths = drill_rig.variable_steels_lengths
-    all_steels_lengths = [installed_steels_length] + variable_steels_lengths
-    installed_resonant_length = drill_rig.installed_resonant_length
-
-    df = acorr_trace.dataframe
-    try:
-        mwd_granularity = acorr_trace.first_global_config.mwd_granularity
-        potential_steels_change_time_intervals = drill_stops_2(df, mwd_granularity)
-    except AttributeError:
-        print("No mwd spacing info!! -- ACORR MUST BE REGENERATED")
-        print("We need a way to handle this in the process_flow.json")
-        print("@Thiago can you help with this?")
-        potential_steels_change_time_intervals = drill_stops(acorr_trace.dataframe,
-                                                             basically_zero_m=0.0017)
-
-    transition_times, transition_depths = get_approximate_transitions(acorr_trace.dataframe,
-                                                         installed_steels_length,
-                                                         variable_steels_lengths,
-                                                         transition_depth_offset_m)
-
-    transition_times, transition_depths = reject_transitions_near_bottom_of_hole(df.depth.max(),
-                                                                                 transition_times,
-                                                                                 transition_depths,
-                                                                                 significant_excess=2.0)
-
-    transition_times, transition_depths = reject_transitions_far_from_drill_stops(transition_times,
-                                                    transition_depths,
-                                                    potential_steels_change_time_intervals,
-                                                    all_steels_lengths)
-
-    transition_times, transition_depths = nearest_time_to_transition_depth(df,
-                                                                          transition_depths,
-                                                                          potential_steels_change_time_intervals)
-
-
-#    plt.figure(1)
-#    color_cyc = 'rgbcmk'
-#    plt.plot(acorr_trace.dataframe.timestamp, acorr_trace.dataframe.depth)
-#    for i, ivl in enumerate(potential_steels_change_time_intervals):
-#        plt.vlines(ivl.lower_bound, 0, 34, color=color_cyc[i])
-#        plt.vlines(ivl.upper_bound, 0, 34, color=color_cyc[i])
-#    for i, td in enumerate(transition_depths):
-#        plt.plot(transition_times[i], td, 'o',  color=color_cyc[i])
-#    plt.show()
-
-    df['drill_string_resonant_length'] = installed_resonant_length
-    resonant_length = installed_resonant_length
-    for i,transition_depth in enumerate(transition_depths):
-        resonant_length += variable_steels_lengths[i]
-        rows_to_update = df['depth'] > transition_depth
-        df.loc[rows_to_update, 'drill_string_resonant_length'] = resonant_length
-
-    return acorr_trace
 
 
 def test(acorr_filename=None):
@@ -330,37 +323,21 @@ def test(acorr_filename=None):
         except ImportError:
             logger.error("you need to specify the path to an acorr file on your machine")
             raise Exception
-        h5_basename = '2380_NS92_82_9409_9409_6172_6172.h5'
-        h5_basename = '2380_NS92_82_9518B_9518B_6172_6172.h5'
-        h5_basename = '2380_NS92_82_9607T_9607T_6172_6172.h5'
-        acorr_filename = os.path.join(line_creek_acorr_folder, h5_basename)
-        h5_basename = 'GUS_DR:R06N:30:GUS:P04:B:T_285784_285784_6674_6674.h5'
-        h5_basename = 'P10_DR:R06N:29:GMS:P10:L:T_N366_286303_6674_6674.h5'
         h5_basename = 'OB_DR:R14N:41:GMS:OB:A:T_B218_286780_6332_6332.h5'#OK
-        #h5_basename = 'P04_DR:R06N:30:GUS:P04:B:T_F185_285755_6674_6674.h5'
         acorr_filename = os.path.join(bma_acorr_folder, h5_basename)
-#        acorr_filename = os.path.join('/home/kkappler', 'tmp', '20190518_RTA72000_PR004.h5')
 
     acorr_trace = TraceData()
     acorr_trace.load_from_h5(acorr_filename)
-    pdb.set_trace()
-    try:
-        mwd_depth_spacing = acorr_trace.first_global_config.mwd_depth_spacing
-    except AttributeError:
-        print("HACK !!! -- ACORR MUST BE REGENERATED")
-        acorr_trace.first_global_config.mwd_depth_spacing = 0.2#m
-        mwd_depth_spacing = acorr_trace.first_global_config.mwd_depth_spacing
-    tmp1 = drill_stops(acorr_trace.dataframe, minimum_stop_duration=60.0, basically_zero_m=0.0017)
-    tmp2 = drill_stops_2(acorr_trace.dataframe, mwd_depth_spacing)
-    pdb.set_trace()
-    acorr_trace = update_acorr_with_resonance_info(acorr_trace)
+#    try:
+#        mwd_depth_spacing = acorr_trace.first_global_config.mwd_depth_spacing
+#    except AttributeError:
+#        print("HACK !!! -- ACORR MUST BE REGENERATED")
+#        mwd_depth_spacing = 0.2#m
+#    tmp1 = drill_stops(acorr_trace.dataframe, minimum_stop_duration=60.0, basically_zero_m=0.0017)
+#    tmp2 = drill_stops_2(acorr_trace.dataframe, mwd_depth_spacing)
+#    #pdb.set_trace()
+#    acorr_trace = update_acorr_with_resonance_info(acorr_trace)
 
-#    pdb.set_trace()
-#    from dcrhino3.process_flow.modules.hybrid.unfold_autocorrelation import unfold_trace_2d
-#    data_array = acorr_trace.component_as_array('axial')
-#    data_array = unfold_trace_2d(data_array)
-#    qqq =np.fft.fft(data_array);
-    print('hi')
 
 
 def main():
