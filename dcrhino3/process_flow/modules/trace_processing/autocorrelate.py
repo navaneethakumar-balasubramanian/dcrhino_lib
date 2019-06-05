b6a440a966e747f736d1483eff18dae0564611bb
Thiago Mourao <xtecox@gmail.com>
2019-06-05 16:21:51 -0300

    faster acorr

--- dcrhino3/process_flow/modules/trace_processing/autocorrelate.py
+++ dcrhino3/process_flow/modules/trace_processing/autocorrelate.py
@@ -1,19 +1,39 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-
+from scipy.signal import correlate
 from dcrhino3.process_flow.modules.trace_processing.base_trace_module import BaseTraceModule
-
+'''
 def autocorrelate_trace(trace_data, n_pts):
         """
         n_pts: integer, this is the number of taps in the decon filter that will be solved for
         WARNING  wants even # points
+        .. :CHANGE: 20190524 modifying this to subtract the mean
         """
 
         zero_time_index = len(trace_data)//2
-
+        dc_offset = np.mean(trace_data)
+        trace_data -= dc_offset #needs to go on the data frame
         acorr = np.correlate(trace_data, trace_data,'same')
         return acorr[zero_time_index:zero_time_index+n_pts]
+'''
+
+
+def autocorrelate_trace(trace_data, n_pts):
+    """
+    FASTER METHOD NEED VALIDATION
+    """
+
+    zero_time_index = len(trace_data) // 2
+    dc_offset = np.mean(trace_data)
+    trace_data -= dc_offset  # needs to go on the data frame
+    # acorr = cross_correlation_using_fft(trace_data,trace_data)
+    # acorr = np.correlate(trace_data, trace_data,'same')
+    acorr = correlate(trace_data, trace_data)
+    # pdb.set_trace()
+    # return acorr[0:n_pts]
+    return acorr[zero_time_index * 2 - 1:zero_time_index * 2 - 1 + n_pts]
+
 
 class AutoCorrelateModule(BaseTraceModule):
     def __init__(self, json, output_path, process_flow,order):
