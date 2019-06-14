c2ae889daf6e934a1d45730d5f80d02bb2e84f64
Thiago Mourao <xtecox@gmail.com>
2019-06-14 14:37:24 -0300

    a

--- bin/sensor_file_to_db.py
+++ bin/sensor_file_to_db.py
@@ -69,37 +69,38 @@ def prepare_to_save(autcorrelated_dataframe,sensor_file_id,original_file_record_
     return autcorrelated_dataframe
 
 def process(list_of_args):
-    file = list_of_args[0]
-    env_config = list_of_args[1]
     try:
+        file = list_of_args[0]
+        env_config = list_of_args[1]
+
         h5f = h5py.File(file, 'r+')
+
+        min_ts = sensor_file_manager.min_ts(h5f)
+        max_ts = sensor_file_manager.max_ts(h5f)
+        # raw_trace_h5_to_db(file,env_config,min_ts,max_ts)
+
+        if sensor_file_manager.is_h5_level0(h5f):
+            h5f.close()
+            print ("TYPE 1 " + str(file))
+            raw_trace_h5_to_db(file, env_config, min_ts, max_ts)
+        else:
+            h5f.close()
+            print ("TYPE 2 " + str(file))
+            acorr_h5_to_db(file, env_config, min_ts, max_ts)
     except:
         file_logger.warn("COULDNT OPEN THIS FILE :" + str(file))
         logger.warn("COULDNT OPEN THIS FILE :" + str(file))
         return
 
-    min_ts = sensor_file_manager.min_ts(h5f)
-    max_ts = sensor_file_manager.max_ts(h5f)
-    # raw_trace_h5_to_db(file,env_config,min_ts,max_ts)
 
-    if sensor_file_manager.is_h5_level0(h5f):
-        h5f.close()
-        print ("TYPE 1 " + str(file))
-        raw_trace_h5_to_db(file, env_config, min_ts, max_ts)
-    else:
-        h5f.close()
-        print ("TYPE 2 " + str(file))
-        acorr_h5_to_db(file, env_config, min_ts, max_ts)
-
-
-def save_to_db(sql_db_helper,h5_file_path,min_ts_df,global_config,min_ts,max_ts,conn,autcorrelated_dataframe,type):
+def save_to_db(sql_db_helper,h5_file_path,min_ts_df,global_config,min_ts,max_ts,conn,autcorrelated_dataframe,file_type):
     logger.info("Adding this file to sensor_files_table")
     original_file_record_day = int(time.strftime("%Y%m%d", time.gmtime(min_ts_df)))
     file_id = sql_db_helper.sensor_files.add(h5_file_path, global_config.rig_id,
                                              str(global_config.sensor_serial_number),
                                              str(global_config.digitizer_serial_number), min_ts,
                                              max_ts,
-                                             json.dumps(vars(global_config), indent=4), type,
+                                             json.dumps(vars(global_config), indent=4), file_type,
                                              status='valid',
                                              file_name=os.path.basename(h5_file_path),
                                              original_file_record_day=original_file_record_day)
