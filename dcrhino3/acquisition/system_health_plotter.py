b99fc8a579364172d8cc44bf4d54e4719be034fa
natal <natal@datacloud.com>
2019-06-17 18:13:46 -0700

    change scales

--- dcrhino3/acquisition/system_health_plotter.py
+++ dcrhino3/acquisition/system_health_plotter.py
@@ -197,13 +197,14 @@ while True:
         delay_plot.plot(np.flipud(delay + drift), 'C0')
         not_nan_drift = drift[~np.isnan(drift)]
         drift_mean = np.mean(not_nan_drift)
+        drift_std = np.std(not_nan_drift)
         print("drift mean", drift_mean)
         delay_plot_twin.plot(np.flipud(drift), 'C7')
-        min, max = delay_plot_twin.get_ylim()
-        if drift_mean >= 0:
-            delay_plot_twin.set_ylim(-1, np.max(not_nan_drift)*1.1)
-        else:
-            delay_plot_twin.set_ylim(np.min(not_nan_drift)*1.1, 1)
+        # min, max = delay_plot_twin.get_ylim()
+        # if drift_mean >= 0:
+        #     delay_plot_twin.set_ylim(-1, np.max(not_nan_drift)*3*drift_std)
+        # else:
+        delay_plot_twin.set_ylim(drift_mean - 3*drift_std, drift_mean + 3*drift_std)
 
         delay_plot.hlines(config.getfloat("SYSTEM_HEALTH_PLOTS", "delay_lower_limit"), 0, length, "y", "dashed")
         delay_plot.hlines(config.getfloat("SYSTEM_HEALTH_PLOTS", "delay_upper_limit"), 0, length, "r", "dashed")
