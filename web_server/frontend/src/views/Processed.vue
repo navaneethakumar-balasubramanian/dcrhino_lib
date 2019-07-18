cf7e14d8ae8d44d0b12b20045832c7fe2ae61c14
Thiago Mourao <xtecox@gmail.com>
2019-07-18 14:49:45 -0700

    a

--- web_server/frontend/src/views/Processed.vue
+++ web_server/frontend/src/views/Processed.vue
@@ -29,8 +29,8 @@
       v-if="!show_dialog_comparison"
     >
     <template slot="headerCell" slot-scope="props">
-      <span v-if="!props.header.clickable" v-on="on" ripple>{{props.header.text}}</span>
-      <v-dialog v-if="props.header.clickable" v-model='props.header.dialog'  max-width="300px">
+      <span v-if="!props.header.values" v-on="on" ripple>{{props.header.text}}</span>
+      <v-dialog v-if="props.header.values" v-model='props.header.dialog'  max-width="300px">
         <template v-slot:activator="{ on }">
   
             <span v-on="on" ripple>{{props.header.text}}</span>
@@ -242,8 +242,8 @@
         let digitizer_ids = [...new Set(processed_list.map(a =>  a.digitizer_id))]
 
         this.headers = [
-                    {text:"Id", value:'processed_hole_id',sortable: false,clickable:false,},
-                    {text:"Processed date", value:'date',sortable: true,clickable:false},
+                    {text:"Id", value:'processed_hole_id',sortable: false},
+                    {text:"Processed date", value:'date',sortable: true},
                     {text:"Bench", value:'bench_name',sortable: false, values:to_unique_obj(bench_names), allChecked:true },
                     {text:"Pattern", value:'pattern_name',sortable: false, values:to_unique_obj(pattern_names), allChecked:true},
                     {text:"Hole_name", value:'hole_name',sortable: false, values:to_unique_obj(hole_names), allChecked:true},
