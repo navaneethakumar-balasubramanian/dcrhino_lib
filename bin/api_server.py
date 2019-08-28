b38c25ca5454724ba15ce64268364ccd9a2d4289
Thiago Mourao <xtecox@gmail.com>
2019-08-28 16:05:42 -0700

    rename

--- bin/api_server.py
+++ bin/api_server.py
@@ -47,7 +47,7 @@ COMPRESS_MIN_SIZE = 500
 Compress(app)
 
 subdomain_name = 'dcmine'
-dataset_name = 'mont_wright_mwd_' + datetime.today().strftime('%Y%m%d')
+dataset_name = 'montwright_rhino_' + datetime.today().strftime('%Y%m%d')
 API_BASE_URL = "http://104.42.216.162:5002/api"
 
 def prop_type_from_column_dtype(column_dtype):
