03151248ffef73bbb6359638c36c93c28f36b6a5
Thiago Mourao <xtecox@gmail.com>
2019-09-18 22:13:49 -0700

    Dev tested with swc and python 3 env

---
+++ web_server/frontend/node_modules/vue2-daterange-picker/src/components/CalendarRanges.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="ranges">
+        <ul v-if="ranges">
+            <li
+                    v-for="(value, key) in ranges"
+                    @click="$emit('clickRange', value)"
+                    :data-range-key="key"
+                    :key="key"
+                    :class="range_class(value)"
+            >{{key}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  import moment from 'moment'
+
+  export default {
+    props: {
+      ranges: Object,
+      selected: Object
+    },
+    methods: {
+      range_class (range) {
+        return { active: moment(this.selected.startDate).isSame(range[0], 'date') && moment(this.selected.endDate).isSame(range[1], 'date') };
+      }
+    },
+  }
+</script>
