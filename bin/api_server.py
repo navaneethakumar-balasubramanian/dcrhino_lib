8a9180ca8a116d55116e55a7f5e17f0791fb4d70
Thiago <xtecox@gmail.com>
2020-05-25 12:13:16 -0700

    print

--- bin/api_server.py
+++ bin/api_server.py
@@ -156,7 +156,7 @@ def update_or_create_config(subdomain_name, dataset_name, df, rhino_props):
         datasets_confs.append(dataset_conf)
 
     token = MWDHelper('').get_token()
-    print("datasetconfs",datasets_confs)
+    print("datasetconfs",dataset_conf)
     deploy_config(token, subdomain_name, dataset_name, datasets_confs)
     return dataset_conf['mapping']
 
